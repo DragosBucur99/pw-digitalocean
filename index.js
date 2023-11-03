4866945e62c443fdcf38e66241c524a7ab316a32
Dragos B <Dragos.Bucur@ibm.com>
2023-11-03 19:13:21 +0200

    Update

--- index.js
+++ index.js
@@ -3,38 +3,6 @@
 // const cors = require("cors");
 // const app = express();
 // const port = process.env.PORT || 3000;
-// const runTests = async (req, res) => {
-//   try {
-//     const child = exec("npx playwright test");
-//     let output = ""; // Variable to store the command output
-//     let errorOutput = "";
-
-//     // Attach event listeners to capture the output
-//     child.stdout?.on("data", (data) => {
-//       output += data;
-//     });
-
-//     child.stderr?.on("data", (data) => {
-//       errorOutput += data; // Log any errors to the console
-//     });
-
-//     // Promise that resolves when the command completes
-//     await new Promise((resolve, reject) => {
-//       child.on("close", (code) => {
-//         if (code !== 0) {
-//           const errorMessage = `playwright return code is non-zero: ${code}\n${errorOutput}`;
-//           reject(errorMessage);
-//         } else {
-//           resolve();
-//         }
-//       });
-//     });
-
-//     res.send(output);
-//   } catch (e) {
-//     res.send(e);
-//   }
-// };
 
 // // Replace these functions with your actual token generation and verification logic.
 // // function generateToken() {
@@ -78,8 +46,45 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const runTests = async (req, res) => {
+  try {
+    const child = exec("npx playwright test");
+    let output = ""; // Variable to store the command output
+    let errorOutput = "";
+
+    // Attach event listeners to capture the output
+    child.stdout?.on("data", (data) => {
+      output += data;
+    });
+
+    child.stderr?.on("data", (data) => {
+      errorOutput += data; // Log any errors to the console
+    });
+
+    // Promise that resolves when the command completes
+    await new Promise((resolve, reject) => {
+      child.on("close", (code) => {
+        if (code !== 0) {
+          const errorMessage = `playwright return code is non-zero: ${code}\n${errorOutput}`;
+          reject(errorMessage);
+        } else {
+          resolve();
+        }
+      });
+    });
+
+    res.send(output);
+  } catch (e) {
+    res.send(e);
+  }
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/tests", (req, res) => {
+  runTests(req, res);
 });
 
 app.listen(port, () => {
