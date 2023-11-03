48d96f92f828b40ce46619611ae3ade90d815a1c
Dragos B <Dragos.Bucur@ibm.com>
2023-11-03 18:34:00 +0200

    Update

--- index.js
+++ index.js
@@ -58,7 +58,6 @@ function requireToken(req, res, next) {
 }
 
 app.use(cors());
-app.use(express.json());
 
 // app.get("/api/test", (req, res) => {
 //   res.json({ message: "This is the /api/test endpoint" });
