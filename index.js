1f0ab54b19859c3e3e628fd031e29e5cea49eadf
Dragos B <Dragos.Bucur@ibm.com>
2023-11-16 19:48:48 +0200

    Update

--- index.js
+++ index.js
@@ -1,96 +1,56 @@
-// const { exec } = require("child_process");
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
-// const port = process.env.PORT || 3000;
+// const runTests = async (req, res) => {
+//   try {
+//     const child = exec("npx playwright test");
+//     let output = ""; // Variable to store the command output
+//     let errorOutput = "";
 
-// // Replace these functions with your actual token generation and verification logic.
-// // function generateToken() {
-// //   return "test";
-// // }
+//     // Attach event listeners to capture the output
+//     child.stdout?.on("data", (data) => {
+//       output += data;
+//     });
 
-// function verifyToken(token) {
-//   return token === "testToken";
-// }
+//     child.stderr?.on("data", (data) => {
+//       errorOutput += data; // Log any errors to the console
+//     });
 
-// // Middleware to check for a valid token
-// function requireToken(req, res, next) {
-//   const token = req.headers["authorization"];
+//     // Promise that resolves when the command completes
+//     await new Promise((resolve, reject) => {
+//       child.on("close", (code) => {
+//         if (code !== 0) {
+//           const errorMessage = `playwright return code is non-zero: ${code}\n${errorOutput}`;
+//           reject(errorMessage);
+//         } else {
+//           resolve();
+//         }
+//       });
+//     });
 
-//   if (!token || !verifyToken(token)) {
-//     return res.status(401).json({ error: "Unauthorized" });
+//     res.json({ data: output });
+//   } catch (e) {
+//     res.json({ error: e });
 //   }
+// };
 
-//   next();
-// }
-
-// app.use(cors());
-
-// // app.get("/api/test", (req, res) => {
-// //   res.json({ message: "This is the /api/test endpoint" });
-// // });
-
-// app.get("/", (req, res) => {
-//   res.send("Hello world!");
+// app.get("/tests", (req, res) => {
+//   runTests(req, res);
 // });
 
-// // app.post("/tests", requireToken, (req, res) => {
-// //   runTests(req, res);
-// // });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
-
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
-const cors = require("cors");
-const { exec } = require("child_process");
 const app = express();
-const port = 3000;
-
-const runTests = async (req, res) => {
-  try {
-    const child = exec("npx playwright test");
-    let output = ""; // Variable to store the command output
-    let errorOutput = "";
-
-    // Attach event listeners to capture the output
-    child.stdout?.on("data", (data) => {
-      output += data;
-    });
-
-    child.stderr?.on("data", (data) => {
-      errorOutput += data; // Log any errors to the console
-    });
-
-    // Promise that resolves when the command completes
-    await new Promise((resolve, reject) => {
-      child.on("close", (code) => {
-        if (code !== 0) {
-          const errorMessage = `playwright return code is non-zero: ${code}\n${errorOutput}`;
-          reject(errorMessage);
-        } else {
-          resolve();
-        }
-      });
-    });
-
-    res.json({ data: output });
-  } catch (e) {
-    res.json({ error: e });
-  }
-};
+const cors = require("cors");
 
 app.use(cors());
 
+app.listen(process.env.PORT, () => {
+  console.log(`App running on :${process.env.PORT}`);
+});
+
 app.get("/", (req, res) => {
-  res.json({ data: "Hello world" });
-});
-
-app.get("/tests", (req, res) => {
-  runTests(req, res);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  return res.status(200).json({
+    ok: true,
+    message: "Playwright server is up and running",
+    status: 200,
+  });
 });
