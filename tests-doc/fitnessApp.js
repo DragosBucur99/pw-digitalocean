9355febb1e428783dd1355a1779f5cfe72fccb7a
Dragos B <Dragos.Bucur@ibm.com>
2024-02-11 20:41:12 +0200

    Add more test cases

--- tests-doc/fitnessApp.js
+++ tests-doc/fitnessApp.js
@@ -1,8 +1,13 @@
 module.exports.testCases = [
   {
     title: "should add food",
-    steps: ["Visit", "Click", "Assert"],
-    expectedOutput: "Should add food to list.",
+    prerequisites: "none",
+    steps: [
+      "Click on the add food button",
+      "Type the name of a valid food e.g. pizza",
+      "Click on the add meal button",
+    ],
+    expectedOutput: "The food should be successfully added to the list.",
     options: [
       {
         name: "What food do you want to add?",
@@ -12,9 +17,107 @@ module.exports.testCases = [
     ],
   },
   {
-    title: "should create a task",
-    steps: ["Visit", "Click", "Assert"],
-    expectedOutput: "Should add task to list.",
+    title: "should edit food",
+    prerequisites: "Have a valid food added to the list.",
+    steps: [
+      "Click on the menu button",
+      "Click on the Edit menu button",
+      "Select food you wish to edit",
+      "Change the value default weight value",
+      "Click on the EDIT button to confirm the action",
+    ],
+    expectedOutput: "The values for weight and nutrients should be updated.",
+    options: [
+      {
+        name: "What food do you want to add?",
+        type: "searchbox",
+        label: "FOOD",
+      },
+      {
+        name: "Amount (grams)",
+        type: "number",
+        label: "FOOD_AMOUNT",
+      },
+    ],
+  },
+  {
+    title: "should delete food",
+    prerequisites: "Have a valid food added to the list.",
+    steps: [
+      "Click on the menu button",
+      "Click on the Delete menu button",
+      "Select food you wish to delete",
+      "Click on the DELETE button to confirm the action",
+    ],
+    expectedOutput: "The food should be removed from the list.",
+    options: [
+      {
+        name: "What food do you want to add?",
+        type: "searchbox",
+        label: "FOOD",
+      },
+    ],
+  },
+  {
+    title: "should create task",
+    prerequisites: "none",
+    steps: [
+      "Click on CREATE TASK button",
+      "Pick a due date",
+      "Write a task name e.g. test",
+      "Choose the priorty level of the task",
+      "Click on Create to-do",
+    ],
+    expectedOutput: "The task should be correctly added to the list.",
+    options: [
+      {
+        name: "Name of the task",
+        type: "searchbox",
+        label: "TASK_NAME",
+      },
+      {
+        name: "Task due date",
+        type: "searchbox",
+        label: "TASK_DUE_DATE",
+      },
+      {
+        name: "Task priority level",
+        type: "dropdown",
+        dropdownValues: ["Low", "Medium", "High"],
+        label: "TASK_PRIORITY_LEVEL",
+      },
+    ],
+  },
+  {
+    title: "should mark task as done",
+    prerequisites: "Have a valid task added to the list.",
+    steps: ["Click on the done button"],
+    expectedOutput:
+      "Should mark task as done and the remaining tasks should be 0.",
+    options: [
+      {
+        name: "Name of the task",
+        type: "searchbox",
+        label: "TASK_NAME",
+      },
+      {
+        name: "Task due date",
+        type: "searchbox",
+        label: "TASK_DUE_DATE",
+      },
+      {
+        name: "Task priority level",
+        type: "dropdown",
+        dropdownValues: ["Low", "Medium", "High"],
+        label: "TASK_PRIORITY_LEVEL",
+      },
+    ],
+  },
+  {
+    title: "should delete task",
+    prerequisites: "Have a valid task added to the list.",
+    steps: ["Click on the task's trash icon"],
+    expectedOutput: "Task should be removed from the list.",
     options: [
       {
         name: "Name of the task",
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,10 @@
 import { test, expect } from "@playwright/test";
 
-test("should add food", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("https://reactfitnessapp-gilt.vercel.app/");
+});
+
+test("should add food", async ({ page }) => {
   await page
     .getByRole("button", { name: "Put some meat on your bones!" })
     .click();
@@ -19,8 +22,77 @@ test("should add food", async ({ page }) => {
   ).toBeVisible();
 });
 
-test("should create a task", async ({ page }) => {
-  await page.goto("https://reactfitnessapp-gilt.vercel.app/");
+test("should edit food", async ({ page }) => {
+  await page
+    .getByRole("button", { name: "Put some meat on your bones!" })
+    .click();
+  await page
+    .getByPlaceholder("What have you eaten?")
+    .fill(process.env.FOOD || "");
+  await Promise.all([
+    page.waitForResponse(
+      (res) => res.status() == 200 && res.url().includes(process.env.FOOD || "")
+    ),
+    page.getByRole("button", { name: "Add meal" }).click(),
+  ]);
+  await expect(
+    page.locator("span").filter({ hasText: process.env.FOOD })
+  ).toBeVisible();
+});
+
+test("should delete food", async ({ page }) => {
+  await page
+    .getByRole("button", { name: "Put some meat on your bones!" })
+    .click();
+  await page
+    .getByPlaceholder("What have you eaten?")
+    .fill(process.env.FOOD || "");
+  await Promise.all([
+    page.waitForResponse(
+      (res) => res.status() == 200 && res.url().includes(process.env.FOOD || "")
+    ),
+    page.getByRole("button", { name: "Add meal" }).click(),
+  ]);
+  await expect(
+    page.locator("span").filter({ hasText: process.env.FOOD })
+  ).toBeVisible();
+});
+
+test("should create task", async ({ page }) => {
+  await page.getByRole("button", { name: "CREATE TASK" }).click();
+  await page.locator("input[placeholder='Pick a due date (optional)']").click();
+  await page.getByText("20", { exact: true }).click();
+  await page
+    .getByText(process.env.TASK_PRIORITY_LEVEL || "", { exact: true })
+    .click();
+  await page
+    .getByPlaceholder("What are you planning?")
+    .fill(process.env.TASK_NAME || "");
+  await page.getByRole("button", { name: "Create to-do" }).click();
+  await expect(
+    page.getByText("remaining tasks: 1", { exact: true })
+  ).toBeVisible();
+  await page.waitForTimeout(2000); // Change this
+});
+
+test("should mark task as done", async ({ page }) => {
+  await page.getByRole("button", { name: "CREATE TASK" }).click();
+  await page.locator("input[placeholder='Pick a due date (optional)']").click();
+  await page.getByText("20", { exact: true }).click();
+  await page
+    .getByText(process.env.TASK_PRIORITY_LEVEL || "", { exact: true })
+    .click();
+  await page
+    .getByPlaceholder("What are you planning?")
+    .fill(process.env.TASK_NAME || "");
+  await page.getByRole("button", { name: "Create to-do" }).click();
+  await expect(
+    page.getByText("remaining tasks: 1", { exact: true })
+  ).toBeVisible();
+  await page.waitForTimeout(2000); // Change this
+});
+
+test("should delete task", async ({ page }) => {
   await page.getByRole("button", { name: "CREATE TASK" }).click();
   await page.locator("input[placeholder='Pick a due date (optional)']").click();
   await page.getByText("20", { exact: true }).click();
